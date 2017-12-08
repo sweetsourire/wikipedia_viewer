b31a85c393e7b4e9bc7860343e4c2bad63b837f9
sweetsourire <bkim1289@gmail.com>
2017-12-08 12:19:17 -0500

    minor update

--- javascript.js
+++ javascript.js
@@ -1,17 +1,11 @@
 $(document).ready(function () {
-
-     $("#results").on("click", ".results", function () {
-         $(this).children(".description:visible").hide("fast");
-         $(this).children(".description:hidden").show("fast");
-     });
-
      var form = document.getElementById("search");
 
      form.onsubmit = function () {
          $("#results").html("");
          $.ajax({
              url: "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + form.keyword.value + "&limit=5&namespace=0&format=json",
-             dataType: 'jsonp',
+             dataType: "jsonp",
              success: function (json) {
                 results = json[1]
                 if(results.length){
@@ -29,4 +23,9 @@ $(document).ready(function () {
                 }               
              });
         };
+
+         $("#results").on("click", ".results", function () {
+         $(this).children(".description:visible").hide("fast");
+         $(this).children(".description:hidden").show("fast");
+        });
     });
